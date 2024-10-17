f65e761462d10c24bf8c758062d0f118104eb147
senethmendis <mendis.seneth@gmail.com>
2024-10-17 17:53:00 +0530

    initial

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const NAVBARITEMS = [
+  { id: 1, name: "Home", link: "/" },
+  { id: 2, name: "Category", link: "/category" },
+  { id: 3, name: "About", link: "/about" },
+  { id: 4, name: "Contact", link: "/contact" },
+];
